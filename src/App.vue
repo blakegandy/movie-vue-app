944f9146988d3e0e865cdb9b0aa8d1922d947a9f
blakegandy <gandyb98@gmail.com>
2020-10-26 11:02:54 -0400

    adds html theme

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <header id="header"></header>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
@@ -10,31 +11,12 @@
       <router-link to="/movies/new">Create Movie</router-link>
     </div>
     <router-view />
+
+    <footer id="footer"></footer>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
 <script>
 export default {
   methods: {
